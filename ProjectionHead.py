9f7817b75578e42f04e2261e9d4012746ea32a52
Катасонов Евгений Алексеевич <eakatasonov_1@edu.hse.ru>
2025-03-23 23:57:12 +0300

    upd

--- ProjectionHead.py
+++ ProjectionHead.py
@@ -29,7 +29,7 @@ class ProjectionHead(nn.Module):
         """
         Perform forward pass, do not forget about skip-connections.
         """
-        proj = self.pro1j(x)
+        proj = self.proj1(x)
         print(proj.shape)
         proj2 = self.proj2(proj)
         out = proj2 + proj
